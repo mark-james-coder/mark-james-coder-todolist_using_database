ba042e717556cb8307b572a09cbff7b0a838ee4f
Mark <markjpoole@yahoo.com>
2021-03-10 21:40:42 +0000

    create db and connect app to it using mongoose

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
-
-const items = ["Do exercise", "Eat breakfast", "Start work"];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+  name: "Welcome to your todo list!"
+});
+
+const item2 = new Item ({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item ({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully saved default items to database.");
+  }
+});
+
 app.get("/", (req, res) => {
 
-  const day = date.getDate();
-
-  res.render("list", {
-    nameOfDay: day, newListItems: items
-  });
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
